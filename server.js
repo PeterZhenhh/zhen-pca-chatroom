b2b53c574a3aeda317df0f18f0aa25ebabe0dde4
PeterZhenhh <2306289410@qq.com>
2020-03-10 20:03:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ socket.on("head-in-line",function(head){
 })
 
 
-
-http.listen(80,localhost,function(){
+const PORT = process.env.PORT || 2333
+http.listen(PORT,localhost,function(){
     console.log("running at 127.0.0.1");
 });
